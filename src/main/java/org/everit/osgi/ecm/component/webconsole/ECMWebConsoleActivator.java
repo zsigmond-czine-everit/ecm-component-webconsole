85f78477ab68af9f6daacf2f1ee621a10234d04f
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2015-01-21 17:57:13 +0100

    Ongoing development

--- src/main/java/org/everit/osgi/ecm/component/webconsole/ECMWebConsoleActivator.java
+++ src/main/java/org/everit/osgi/ecm/component/webconsole/ECMWebConsoleActivator.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Everit - ECM Component Webconsole.
+ *
+ * Everit - ECM Component Webconsole is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - ECM Component Webconsole is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - ECM Component Webconsole.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.everit.osgi.ecm.component.webconsole;
 
 import java.util.Hashtable;
@@ -8,6 +24,7 @@ import org.everit.osgi.ecm.component.resource.ComponentContainer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.framework.wiring.BundleWiring;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class ECMWebConsoleActivator implements BundleActivator {
@@ -21,6 +38,7 @@ public class ECMWebConsoleActivator implements BundleActivator {
         servletProps.put("felix.webconsole.label", "everit_ecm_component");
         servletProps.put("felix.webconsole.category", "Everit");
         servletProps.put("felix.webconsole.title", "ECM Components");
+        servletProps.put("felix.webconsole.css", "res/ui/config.css");
 
         @SuppressWarnings({ "unchecked", "rawtypes" })
         Class<ComponentContainer<?>> clazz = (Class) ComponentContainer.class;
@@ -28,7 +46,8 @@ public class ECMWebConsoleActivator implements BundleActivator {
         containerTracker = new ServiceTracker<ComponentContainer<?>, ComponentContainer<?>>(context, clazz, null);
         containerTracker.open();
 
-        Servlet servlet = new ECMWebConsoleServlet(containerTracker);
+        ClassLoader classLoader = context.getBundle().adapt(BundleWiring.class).getClassLoader();
+        Servlet servlet = new ECMWebConsoleServlet(containerTracker, classLoader);
         servletSR = context.registerService(Servlet.class, servlet, servletProps);
     }
 
--- src/main/java/org/everit/osgi/ecm/component/webconsole/ECMWebConsoleServlet.java
+++ src/main/java/org/everit/osgi/ecm/component/webconsole/ECMWebConsoleServlet.java
@@ -1,10 +1,28 @@
+/**
+ * This file is part of Everit - ECM Component Webconsole.
+ *
+ * Everit - ECM Component Webconsole is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - ECM Component Webconsole is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - ECM Component Webconsole.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.everit.osgi.ecm.component.webconsole;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Map.Entry;
-import java.util.Set;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.SortedMap;
 
 import javax.servlet.ServletException;
@@ -12,9 +30,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.everit.expression.ExpressionCompiler;
+import org.everit.expression.ParserConfiguration;
+import org.everit.expression.mvel.MvelExpressionCompiler;
 import org.everit.osgi.ecm.component.resource.ComponentContainer;
-import org.everit.osgi.ecm.component.resource.ComponentRevision;
-import org.everit.osgi.ecm.metadata.ComponentMetadata;
+import org.everit.templating.CompiledTemplate;
+import org.everit.templating.html.HTMLTemplateCompiler;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
 
@@ -25,10 +46,24 @@ public class ECMWebConsoleServlet extends HttpServlet {
      */
     private static final long serialVersionUID = -187515087668802084L;
 
+    private final ClassLoader classLoader;
+
+    private final CompiledTemplate componentsTemplate;
+
     private final ServiceTracker<ComponentContainer<?>, ComponentContainer<?>> containerTracker;
 
-    public ECMWebConsoleServlet(final ServiceTracker<ComponentContainer<?>, ComponentContainer<?>> containerTracker) {
+    public ECMWebConsoleServlet(final ServiceTracker<ComponentContainer<?>, ComponentContainer<?>> containerTracker,
+            final ClassLoader classLoader) {
         this.containerTracker = containerTracker;
+        this.classLoader = classLoader;
+
+        ExpressionCompiler expressionCompiler = new MvelExpressionCompiler();
+        HTMLTemplateCompiler htmlTemplateCompiler = new HTMLTemplateCompiler(expressionCompiler);
+
+        ParserConfiguration parserConfiguration = new ParserConfiguration(classLoader);
+        componentsTemplate = htmlTemplateCompiler.compile(readResource("META-INF/webcontent/ecm_components.html"),
+                parserConfiguration);
+
     }
 
     @Override
@@ -42,37 +77,57 @@ public class ECMWebConsoleServlet extends HttpServlet {
         SortedMap<ServiceReference<ComponentContainer<?>>, ComponentContainer<?>> tracked = containerTracker
                 .getTracked();
 
-        Set<Entry<ServiceReference<ComponentContainer<?>>, ComponentContainer<?>>> trackedEntries = tracked.entrySet();
+        Map<String, Object> vars = new HashMap<String, Object>();
+        vars.put("ccMap", tracked);
+        vars.put("appRoot", appRoot);
 
-        writer.write("<table>");
+        componentsTemplate.render(writer, vars, "content");
+        // writer.write("<table>");
+        //
+        // for (Entry<ServiceReference<ComponentContainer<?>>, ComponentContainer<?>> entry : trackedEntries) {
+        // ServiceReference<ComponentContainer<?>> serviceReference = entry.getKey();
+        // Object serviceId = serviceReference.getProperty("service.id");
+        // ComponentContainer<?> componentContainer = entry.getValue();
+        // ComponentMetadata componentMetadata = componentContainer.getComponentMetadata();
+        //
+        // writer.write("<tr>");
+        // writer.write("<td><a href=\"" + appRoot + "/services/" + serviceId + "\">" + serviceId + "</a></td>");
+        //
+        // writer.write("<td>" + componentMetadata.getComponentId() + "</td>");
+        // writer.write("</tr>");
+        //
+        // ComponentRevision[] componentRevisions = componentContainer.getComponentRevisions();
+        // if (componentRevisions.length > 0) {
+        // writer.write("<tr>");
+        // for (ComponentRevision componentRevision : componentRevisions) {
+        // writer.write("<td>" + componentRevision.getState() + "</td>");
+        // if (componentRevision.getCause() != null) {
+        // StringWriter sw = new StringWriter();
+        // PrintWriter pw = new PrintWriter(sw);
+        // componentRevision.getCause().printStackTrace(pw);
+        // writer.write("<td><pre>" + sw.toString() + "</pre></td>");
+        // }
+        // }
+        // writer.write("</tr>");
+        // }
+        // }
+        // writer.write("</table>");
+    }
 
-        for (Entry<ServiceReference<ComponentContainer<?>>, ComponentContainer<?>> entry : trackedEntries) {
-            ServiceReference<ComponentContainer<?>> serviceReference = entry.getKey();
-            Object serviceId = serviceReference.getProperty("service.id");
-            ComponentContainer<?> componentContainer = entry.getValue();
-            ComponentMetadata componentMetadata = componentContainer.getComponentMetadata();
-
-            writer.write("<tr>");
-            writer.write("<td><a href=\"" + appRoot + "/services/" + serviceId + "\">" + serviceId + "</a></td>");
-
-            writer.write("<td>" + componentMetadata.getComponentId() + "</td>");
-            writer.write("</tr>");
-
-            ComponentRevision[] componentRevisions = componentContainer.getComponentRevisions();
-            if (componentRevisions.length > 0) {
-                writer.write("<tr>");
-                for (ComponentRevision componentRevision : componentRevisions) {
-                    writer.write("<td>" + componentRevision.getState() + "</td>");
-                    if (componentRevision.getCause() != null) {
-                        StringWriter sw = new StringWriter();
-                        PrintWriter pw = new PrintWriter(sw);
-                        componentRevision.getCause().printStackTrace(pw);
-                        writer.write("<td><pre>" + sw.toString() + "</pre></td>");
-                    }
-                }
-                writer.write("</tr>");
+    private String readResource(final String resourceName) {
+        InputStream inputStream = classLoader.getResourceAsStream(resourceName);
+        byte[] buf = new byte[1024];
+        try {
+            int r = inputStream.read(buf);
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            while (r > -1) {
+                bout.write(buf, 0, r);
+                r = inputStream.read(buf);
             }
+            return new String(bout.toByteArray(), Charset.forName("UTF8"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        writer.write("</table>");
+
     }
 }
